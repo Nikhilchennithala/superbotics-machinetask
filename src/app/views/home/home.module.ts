22d6c3d5d1418c627597b6397582f780794d9958
Nikhil <nikhil@antiliasolutions.com>
2023-04-26 12:12:45 +0530

    first

---
+++ src/app/views/home/home.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FoodListComponent } from './food-list/food-list.component';
+
+
+
+@NgModule({
+  declarations: [FoodListComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class HomeModule { }
